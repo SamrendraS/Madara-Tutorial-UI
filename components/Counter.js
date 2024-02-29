267c45da843aaedf3cdd3e9d673147c64af3ddef
SamrendraS <samrendrakumarsingh01@gmail.com>
2024-02-29 15:53:31 +0530

    adding UI files

---
+++ components/Counter.js
@@ -0,0 +1,64 @@
+'use client'
+const { useState } = require("react")
+import useLoader from '@/hooks/useLoader';
+import useWallet from '@/hooks/useWallet';
+import { Contract, RpcProvider } from 'starknet';
+
+// TODO: Replace CONTRACT_ADDRESS with your deployed contract address 
+const CONTRACT_ADDRESS = "0x15796dc2a3587aff889bf4fef924c6e77b1f35cd5619ee835c0366cf8af40c0";
+const contractABI = require("../lib/ContractABI.json");
+const rpcProvider = new RpcProvider({ nodeUrl: "http://localhost:9944" });
+const contract = new Contract(contractABI.abi, CONTRACT_ADDRESS, rpcProvider);
+
+const Counter = () => {
+    const [count, setCount] = useState(null);
+    const { loading, startLoading, stopLoading } = useLoader();
+    const { wallet, walletFns } = useWallet(rpcProvider);
+    const { connection } = wallet;
+
+    const fetchCount = async () => {
+        if (!contract?.address) return;
+        const count = await contract.get_current_count();
+        setCount(Number(count));
+    }
+    fetchCount();
+
+    async function executeTxn(_) {
+        if (!connection || !_?.target?.id) return;
+        startLoading();
+        const calls = [{
+            contractAddress: CONTRACT_ADDRESS,
+            entrypoint: _.target.id,
+            calldata: []
+        }]
+        try {
+            const tx = await connection.account.execute(calls);
+            await rpcProvider.waitForTransaction(tx.transaction_hash, { retryInterval: 1000 });
+            fetchCount();
+        } catch (error) {
+            alert("Error: User rejected transaction");
+        } finally {
+            stopLoading();
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            {loading && <div>Loading...</div>}
+            <div className="m-4">
+                <span className="span: text-6xl font-bold">{count}</span>
+            </div>
+            <div className="flex space-x-4 mt-4">
+                <button onClick={executeTxn} id="increment" className={`bg-blue-500 text-white px-4 py-2 rounded ${!connection || loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={!connection || loading}>Increment</button>
+                <button onClick={executeTxn} id="decrement" className={`bg-amber-500 text-white px-4 py-2 rounded ${!connection || loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={!connection || loading}>Decrement</button>
+            </div>
+            <div className='flex space-x-4 mt-24'>
+                {connection ?
+                    <button className='bg-red-500 text-white px-4 py-2 rounded w-full' onClick={walletFns.disconnectWallet} style={{ fontSize: '1.5rem' }}>Disconnect Wallet</button> :
+                    <button className='bg-green-500 text-white px-4 py-2 rounded w-full' onClick={walletFns.connectWallet} style={{ fontSize: '1.5rem' }}>Connect Wallet</button>
+                }
+            </div>
+        </div>
+    )
+}
+export default Counter;
